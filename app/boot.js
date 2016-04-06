f27fb10eda75c7207e808fb5fcec13b94463c9ea
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-04-07 00:30:12 +0200

    Enable prod mode

--- app/boot.js
+++ app/boot.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './profile.component'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './profile.component', 'angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, profile_component_1;
+    var browser_1, profile_component_1, core_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -9,8 +9,12 @@ System.register(['angular2/platform/browser', './profile.component'], function(e
             },
             function (profile_component_1_1) {
                 profile_component_1 = profile_component_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             browser_1.bootstrap(profile_component_1.ProfileComponent);
         }
     }
--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {ProfileComponent} from './profile.component'
+import {enableProdMode} from 'angular2/core';
 
+enableProdMode();
 bootstrap(ProfileComponent);
