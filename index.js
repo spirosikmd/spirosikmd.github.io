64aebf162a72348e2849e9a7b38bf32699078399
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-12-11 20:14:50 +0100

    Removed console logs and fixed closing animation to run only when clicking out of the box

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-var socialLinks = document.querySelector('.social-links');
+var socialLinksWrapper = document.querySelector('.social-links');
+var socialLinks = document.querySelectorAll('.social-link');
+var socialLinkIcons = document.querySelectorAll('.social-link__icon');
 var linkChooser = document.querySelector('.link-chooser');
 
 function show () {
-  socialLinks.style.display = 'block';
-  socialLinks.style.animationName = 'stretch-in';
+  socialLinksWrapper.style.display = 'block';
+  socialLinksWrapper.style.animationName = 'stretch-in';
 
   linkChooser.style.display = 'none';
 }
 
 function hide () {
-  socialLinks.style.display = 'none';
+  socialLinksWrapper.style.display = 'none';
 
   linkChooser.style.display = 'block';
   linkChooser.style.animationName = 'stretch-out';
 }
 
-window.addEventListener('click', function () {
-  //Hide the menus if visible
-  console.log('Hide the menus if visible');
-  if (socialLinks.style.display === 'block') {
-    hide();
+window.addEventListener('click', function (event) {
+  console.log(event.target);
+  if (event.target === socialLinksWrapper) {
+    return;
   }
+  if (socialLinksWrapper.style.display !== 'block') {
+    return;
+  }
+  var isSocialLinkOrIcon = false;
+  socialLinks.forEach(function (socialLink) {
+    if (isSocialLinkOrIcon) {
+      return;
+    }
+    if (socialLink === event.target) {
+      isSocialLinkOrIcon = true;
+    }
+  });
+  if (isSocialLinkOrIcon) {
+    return;
+  }
+  socialLinkIcons.forEach(function (socialLinkIcon) {
+    if (isSocialLinkOrIcon) {
+      return;
+    }
+    if (socialLinkIcon === event.target) {
+      isSocialLinkOrIcon = true;
+    }
+  });
+  if (isSocialLinkOrIcon) {
+    return;
+  }
+
+  hide();
 });
 
 linkChooser.addEventListener('click', function (event) {
   event.stopPropagation();
-  console.log('social links click');
   show();
 });
