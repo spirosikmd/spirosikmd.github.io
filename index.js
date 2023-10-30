ecaec762598129da20a50ea6d043fdee3fe3560e
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2023-10-30 18:35:09 +0200

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-(function() {
+(function () {
   var currentYear = new Date().getFullYear().toString();
-  var yearElement = document.getElementById('year');
+  var yearElement = document.getElementById("year");
   yearElement.innerHTML = currentYear;
 })();
